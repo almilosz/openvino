3eb52363a8fc4ed8d317df3ba630f0da7934bd46
almilosz <alicja.miloszewska@intel.com>
2024-02-09 10:28:04 +0100

    Add test class and script

--- src/bindings/js/node/index_debug.js
+++ src/bindings/js/node/index_debug.js
@@ -1,8 +1,47 @@
-const testAddon = require('./build/Debug/ov_node_addon.node');
+const testAddon = require('./build/Release/ov_node_addon.node');
+console.log("Module initialized.\n\n")
 
-const core = new testAddon.Core();
-const model = core.readModel('absolute-path-to-model');
 
-console.log(model.getName())
+function testMethodA() {
+    const t = testAddon.methodA();
+    if (t instanceof testAddon.Basic) {
+        console.log("Yes, it's BasicWrap")
+    }
+    console.log("Exiting methodA test \n");
+}
+testMethodA()
+
+// Check C++ custom class destructors. -------------------------------
+function testBasicWrap(iter) {
+    const t_obj = new testAddon.Basic();
+
+    if(iter % 100_000 == 0) {
+    console.log(`Exiting testBasicWrap ${t_obj.getMessage()}`);
+    }
+}
+
+for (let i = 0; i<5_000_000; i++) {
+    testBasicWrap(i);
+    if (i % 1000 == 0) {
+        console.log(`Test heapUsed using node-addon ObjectWrap ${process.memoryUsage().heapUsed/1000000} ${process.memoryUsage().heapTotal/1000000}MB\n`) 
+
+    }
+}
+    
+// -------------------------------------------------------------------
+
+
+
+function testMethodB() {
+    const t = testAddon.methodB();
+    if (t instanceof testAddon.Basic) {
+        console.log("Yes, it's BasicWrap")
+    }
+    console.log("Exiting methodB test \n");
+}
+testMethodB()
+testMethodA()
+
 
 module.exports = testAddon;
+console.log("\n\nTHE END \n\n")
---
+++ src/bindings/js/node/src/basic_class.hpp
@@ -0,0 +1,70 @@
+#include <napi.h>
+#include <addon.hpp>
+
+Napi::Value MethodA(const Napi::CallbackInfo& info) {
+    printf("MethodA\n");
+    Napi::FunctionReference& constructor = info.Env().GetInstanceData<AddonData>()->basic_wrap;
+    return constructor.New({});
+}
+
+Napi::Value MethodB(const Napi::CallbackInfo& info) {
+    printf("MethodB\n");
+    Napi::HandleScope scope(info.Env());
+    Napi::FunctionReference& constructor = info.Env().GetInstanceData<AddonData>()->basic_wrap;
+    
+    // https://github.com/nodejs/node-addon-api/blob/main/doc/object_lifetime_management.md#making-handle-lifespan-shorter-than-that-of-the-native-method
+    for (int i = 0; i < 50000; i++) {
+        Napi::HandleScope scope(info.Env()); // Case 
+        auto b = constructor.New({});
+        std::string name = std::string("inner-scope") + std::to_string(i);
+        Napi::Value newValue = Napi::String::New(info.Env(), name.c_str());
+        // do something with newValue
+        
+    };
+
+    // auto sentence1 = Napi::String::New(info.Env(), "no 1");
+    // auto sentence2 = Napi::String::New(scope.Env(), "no 2");
+    return constructor.New({});
+}
+
+
+class BasicClass {
+public:
+    BasicClass(): _msg("New message.\n"){
+        counter++;
+        printf("%d New BasicClass has been initialized.\n", counter);
+                
+    }
+    ~BasicClass() {
+        printf("%d BasicClass has been destroyed.\n", counter);
+        counter--;
+    }
+    std::string _msg;
+    static int counter;
+};
+int BasicClass::counter = 0;
+
+class BasicWrap : public Napi::ObjectWrap<BasicWrap> {
+public:
+    BasicWrap(const Napi::CallbackInfo& info) : Napi::ObjectWrap<BasicWrap>(info) {
+        this->_basic_class._msg = "Basic Class constructor\n";
+        printf("BasicWrap created.\n");
+    }
+
+    ~BasicWrap() {
+        printf("BasicWrap destroyed.\n");
+    }
+
+    static Napi::Function get_class(Napi::Env env) {
+        return DefineClass(env, "BasicWrap", {
+            InstanceMethod("getMessage", &BasicWrap::get_message)
+        });
+    }
+
+    Napi::Value get_message(const Napi::CallbackInfo& info) {
+        return Napi::String::New(info.Env(), _basic_class._msg);
+    }
+
+private:
+    BasicClass _basic_class;
+};
